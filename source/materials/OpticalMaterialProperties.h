68fe3f81a3930d1879ddf4cdee398156e1904702
Paola Ferrario <paola.ferrario@gmail.com>
2020-06-16 18:55:11 +0200

    Clean up optical material properties.
    Some properties have been removed, some deprecated. All of them have
    been adjusted to a fixed range of energies: 1 - 8.21 eV, except for
    argon properties. The TPB properties have been updated to the most
    recent measurements.

--- source/materials/OpticalMaterialProperties.h
+++ source/materials/OpticalMaterialProperties.h
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
 ///  \file   OpticalMaterialProperties.h
 ///  \brief  Optical properties of most used materials.
-///  
+///
 ///  \author  <justo.martin-albo@ific.uv.es>
 ///           <francesc.monrabal4@ific.uv.es>
 ///           <paola.ferrario@ific.uv.es>
@@ -26,7 +26,7 @@ namespace nexus {
 
   using namespace CLHEP;
 
-  /// Optical properties of several materials. 
+  /// Optical properties of several materials.
   /// (N.B. This is a stateless class where all methods are static functions.)
 
   class OpticalMaterialProperties
@@ -43,53 +43,44 @@ namespace nexus {
     /// ultraviolet down to 160 nm
     static G4MaterialPropertiesTable* FusedSilica();
 
-    static G4MaterialPropertiesTable* FakeFusedSilica(G4double transparency=.9, 
-						      G4double thickness=1.*mm);
-    
     static G4MaterialPropertiesTable* Sapphire();
-
-    static G4MaterialPropertiesTable* OptCoupler();
 
     static G4MaterialPropertiesTable* GAr(G4double sc_yield);
     static G4MaterialPropertiesTable* LAr();
 
-    static G4MaterialPropertiesTable* GXe(G4double pressure=1.*bar, 
+    static G4MaterialPropertiesTable* GXe(G4double pressure=1.*bar,
 					  G4double temperature=STP_Temperature,
 					  G4int sc_yield=25510/MeV);
 
-    static G4MaterialPropertiesTable* FakeGrid(G4double pressure=1.*bar, 
-					       G4double temperature=STP_Temperature, 
-					       G4double transparency=.9, 
+    static G4MaterialPropertiesTable* FakeGrid(G4double pressure=1.*bar,
+					       G4double temperature=STP_Temperature,
+					       G4double transparency=.9,
 					       G4double thickness=1.*mm,
 					       G4int sc_yield=25510/MeV);
 
-   static G4MaterialPropertiesTable* LXe();
-   static G4MaterialPropertiesTable* FakeLXe();
-   static G4MaterialPropertiesTable* LXe_nconst();
-   static G4MaterialPropertiesTable* LXe_window();
+    static G4MaterialPropertiesTable* LXe();
+    static G4MaterialPropertiesTable* FakeLXe();
+    static G4MaterialPropertiesTable* LXe_nconst();
 
-
-    static G4MaterialPropertiesTable* Glass();
-    
-    static G4MaterialPropertiesTable* TPB(G4double pressure=10.*bar, 
-					  G4double temperature=STP_Temperature);
-    static G4MaterialPropertiesTable* TPB_LXe(G4double decay=2.2*nanosecond);
-    static G4MaterialPropertiesTable* TPB_LXe_nconst(G4double decay=2.2*nanosecond);
-
-    static G4MaterialPropertiesTable* TPBOld();
-    static G4MaterialPropertiesTable* TPBmatr();
-    static G4MaterialPropertiesTable* WLS2();
     static G4MaterialPropertiesTable* PTFE();
     static G4MaterialPropertiesTable* PTFE_LXe(G4double reflectivity=0.95);
-    static G4MaterialPropertiesTable* PTFE_with_TPB();
     static G4MaterialPropertiesTable* PTFE_non_reflectant();
+
+    static G4MaterialPropertiesTable* TPB(G4double decay=2.2*nanosecond);
+    static G4MaterialPropertiesTable* TPB_LXe_nconst(G4double decay=2.2*nanosecond);
+
     static G4MaterialPropertiesTable* LYSO();
     static G4MaterialPropertiesTable* LYSO_nconst();
     static G4MaterialPropertiesTable* FakeLYSO();
     static G4MaterialPropertiesTable* ReflectantSurface(G4double reflectivity=0.95);
 
-  
+
   private:
+
+    static constexpr G4double optPhotMinE_ = 1.   * eV;
+    static constexpr G4double optPhotMaxE_ = 8.21 * eV;
+    static constexpr G4double noAbsLength_ = 1.e8  * m;
+
     /// Constructor (hidden)
     OpticalMaterialProperties();
     /// Destructor (hidden)
